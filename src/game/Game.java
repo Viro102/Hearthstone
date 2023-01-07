bb99bd9bce4837e203e70d5218f96a3070244f24
Adam Virostek <adam.virostek@gmail.com>
2023-01-07 13:19:42 +0100

    game logic somewhat close

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,17 +1,22 @@
 package game;
 
-import gui.*;
+import gui.Panel;
+import gui.Frame;
 
 public class Game {
+    private static final int PLAYER_MAX_HP = 20;
     private Player[] players;
     private Panel panel;
+    private Card selectedCard;
 
     public Game() {
         this.players = new Player[2];
-        this.players[0] = new Player(30, 0, new Deck(), new Table());
-        this.players[1] = new Player(30, 0, new Deck(), new Table());
+        this.players[0] = new Player(PLAYER_MAX_HP, 0);
+        this.players[1] = new Player(PLAYER_MAX_HP, 1);
 
         this.players[0].setTurn(true);
+        this.players[0].setMana(1);
+        this.players[1].setMana(10);
 
         this.panel = new Panel(this);
         new Frame(panel);
@@ -21,10 +26,9 @@ public class Game {
         for (int i = 0; i < 3; i++) {
             int x = i * 170;
             var card = this.players[0].drawCard();
-            card.setPosition(20 + x, 500);
-            this.panel.renderCard(card);
+            card.setPosition(30 + x, 530);
         }
-        this.players[0].setMana(1);
+        this.panel.test();
         Logging.printStateAll(players);
     }
 
@@ -34,10 +38,10 @@ public class Game {
                 pl.setTurn(false);
                 continue;
             }
-            var drawnCard = pl.drawCard();
-            this.panel.renderCard(drawnCard);
-            pl.setMana(pl.getMana() + 1);
             pl.setTurn(true);
+            pl.setMana(pl.getMana() + 1);
+            pl.drawCard();
+            this.panel.test();
         }
         System.out.println("Turn ended");
         Logging.printStatePlayers(players);
@@ -46,57 +50,63 @@ public class Game {
     private void specialCardBuff(Player player, Card card) {
         if (card.getType().equals("buff")) {
             int buffAmout = card.getBuffAmount();
-            for (var k : player.getTable().getCards()) {
-                k.setHp(k.getHp() + buffAmout);
-                k.setDamage(k.getDamage() + buffAmout);
-            }
-        }
-    }
-
-    public Card playACard(Player player, String card) {
-        for (var c : player.getCards()) {
-            if (c.getName().equals(card)) {
-                if (player.playCard(c)) {
-                    this.specialCardBuff(player, c);
-                    this.panel.renderCard(c);
-                    return c;
-                } else {
-                    return null;
+            for (var c : player.getBoard().getCards()) {
+                if (c == null) {
+                    continue;
                 }
+                c.setHp(c.getHp() + buffAmout);
+                c.setDamage(c.getDamage() + buffAmout);
             }
         }
-
-        System.out.println("Card not found");
-        return null;
     }
 
-    public Card playACard(Player player, Card card) {
-        var success = player.playCard(card);
-        if (success) {
+    public void playACard(Player player, int i) {
+        var card = player.playCard(i);
+        if (card != null) {
             this.specialCardBuff(player, card);
-            this.panel.renderCard(card);
-            return card;
-        } else {
-            return null;
+            this.panel.test();
         }
     }
 
-    public Card selectCardInPlay(Player player, Card card) {
-        return player.getTable().getCard(card);
+    public void selectCard(Player player, int i) {
+        if (selectedCard == player.getBoard().getCard(i)) {
+            this.selectedCard = null;
+            this.panel.removeGlow(i);
+        } else {
+            this.selectedCard = player.getBoard().getCard(i);
+            this.panel.renderGlow(i);
+        }
     }
 
-    public void attack(Player player, Card source, Card target) {
-        target.setHp(target.getHp() - selectCardInPlay(player, source).getDamage());
+    public void attack(int i) {
+        if (this.selectedCard == null) {
+            System.out.println("No card selected");
+            return;
+        }
+        var player = this.getOffTurnPlayer();
+        var target = player.getBoard().getCard(i);
+        target.setHp(target.getHp() - this.selectedCard.getDamage());
     }
 
-    public void attackFace(Player player, Card card, Player target) {
-        for (var c : player.getTable().getCards()) {
+    public void attackFace() {
+        var target = this.getOffTurnPlayer();
+        var attacker = this.getOnTurnPlayer();
+
+        if (this.selectedCard == null) {
+            System.out.println("No card selected");
+            return;
+        }
+
+        for (var c : attacker.getBoard().getCards()) {
+            if (c == null) {
+                continue;
+            }
             if (c.getType().equals("taunt")) {
                 System.out.println("Taunt card in play, cannot attack face");
                 return;
             }
         }
-        target.setHp(target.getHp() - selectCardInPlay(player, card).getDamage());
+        target.setHp(target.getHp() - this.selectedCard.getDamage());
         this.isGameOver();
     }
 
@@ -109,11 +119,26 @@ public class Game {
     }
 
     public Player getOnTurnPlayer() {
-        for (var pl : this.players) {
-            if (pl.isTurn()) {
-                return pl;
+        for (var player : this.players) {
+            if (player.isTurn()) {
+                return player;
             }
         }
+        System.out.println("No player on turn");
         return null;
+    }
+
+    public Player getOffTurnPlayer() {
+        for (var player : this.players) {
+            if (!player.isTurn()) {
+                return player;
+            }
+        }
+        System.out.println("No player off turn");
+        return null;
+    }
+
+    public Player[] getPlayers() {
+        return this.players;
     }
 }
