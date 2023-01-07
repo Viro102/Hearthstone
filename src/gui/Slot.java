c454963d31e7726abb59ef6280de56b3387ad20d
Adam Virostek <adam.virostek@gmail.com>
2023-01-07 03:22:11 +0100

    implement slot system

---
+++ src/gui/Slot.java
@@ -0,0 +1,63 @@
+package gui;
+
+import java.awt.Rectangle;
+
+public class Slot {
+    private boolean free;
+    private boolean glow;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int id;
+
+    public Slot(int id, int x, int y) {
+        this.free = true;
+        this.glow = false;
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.width = 150;
+        this.height = 200;
+    }
+
+    public boolean isGlow() {
+        return this.glow;
+    }
+
+    public boolean isFree() {
+        return this.free;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public Rectangle getShape() {
+        return new Rectangle(this.x, this.y, this.width, this.height);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setGlow(boolean glow) {
+        this.glow = glow;
+    }
+
+    public void setFree(boolean free) {
+        this.free = free;
+    }
+}
